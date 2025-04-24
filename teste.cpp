203c6982f55e9cda18027991a3dd6c4e2ffaad6d
profefbarreto <profefbarreto@gmail.com>
2025-04-24 18:11:55 -0300

    nome e sobrenome

--- teste.cpp
+++ teste.cpp
@@ -5,10 +5,12 @@ int main(){
 using namespace std;
 
     string nome = "Eduardo";
+    string sobrenome = "Barreto";
 
     //O comentário é uma boa prática!
-    cout<<"Meu nome é: " + nome;
+    cout<<"Meu nome é: " + nome + " " + sobrenome;
     cout<<"\nObrigado!";
+    //"\n"
 
     return 0;
 }
