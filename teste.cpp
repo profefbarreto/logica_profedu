647d8f7986ebda049c4fbc3c0c3c743d481311a5
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-04-25 10:40:46 -0300

    soma e multiplicação

--- teste.cpp
+++ teste.cpp
@@ -4,23 +4,13 @@ int main(){
 
 using namespace std;
 
-    double a = 3;
-    double b = 2;
+    int a = 4;
+    int b = 2;
     int soma;
-    int subt;
-    int mult;
-    double div;
 
-    soma = a + b;
-    subt = a - b;
-    mult = a * b;
-    div = a / b;
+    soma = (a + b)*2;
 
     cout<<soma;
-    cout<<"\n"<<subt;
-    cout<<"\n"<<mult;
-    cout<<"\n"<<div;
-    cout<<"\n";
 
 
     return 0;
