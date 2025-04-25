9d3438bf2ad8e493b8e5c33955975822ac403bae
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-04-25 10:33:18 -0300

    brincando com as char

--- teste.cpp
+++ teste.cpp
@@ -3,11 +3,22 @@
 int main(){
 
 using namespace std;
-
+    /*
     char a = 'E';
     char b = 'd';
     char c = 'u';
     cout<<a+b+c;
+    //O que aconteceu?
+    //para concatenar, seria ideal usar strings e não char
+    //ou então fazer desse jeito que será mostrado
+    */
+    
+    char a = 'E';
+    char b = 'd';
+    char c = 'u';
+
+    cout<<a<<b<<c;
+
 
     return 0;
 }
