8e35f1b966796e331aac24d0ed40fef846adca0b
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-04-28 10:13:28 -0300

    operadores

--- teste.cpp
+++ teste.cpp
@@ -4,9 +4,11 @@ int main(){
 
 using namespace std;
 
-    int x = 100 + 50;
+    int soma1 = 100 + 150;
+    int soma2 = soma1 + 250;
+    int soma3 = soma2 + soma2;
 
-    cout<<x<<"\n";
+    cout<<soma3<<"\n";
 
 
     return 0;
