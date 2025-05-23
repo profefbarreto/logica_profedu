cc2451cdfe088721f611cee6cf9e60a45233436f
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-23 20:53:48 -0300

    logical

--- teste.cpp
+++ teste.cpp
@@ -1,48 +1,15 @@
 #include<iostream>
-#include<string>
-#include<iomanip>
-#include<locale>
 
-class Produto{
-    public:
-        std::string nome;
-        double preco;
-        int quantidade;
-        double total;
-
-    Produto(){}
-
-    virtual void receberInfo(){
-        std::cout<<"Nome do produto: "<<std::endl;
-        std::cin>>nome;
-        std::cout<<"Preço do produto: "<<std::endl;
-        std::cin>>preco;
-        std::cout<<"Quantidade do produto: "<<std::endl;
-        std::cin>>quantidade;
-    }
-
-    virtual void detalhes(){
-        std::cout<<"Nome do produto: "<<nome<<std::endl;
-        std::cout<<"Valor do produto: R$ "<<std::fixed<<std::setprecision(2)<<preco<<std::endl;
-        std::cout<<"Quantidade do produto: "<<quantidade<<std::endl;
-        calcular();
-        std::cout<<std::endl;
-    }
-
-    virtual void calcular(){
-        total = preco * quantidade;
-        std::cout<<"O valor total será de: R$ "<<std::fixed<<std::setprecision(2)<<total<<std::endl;
-        std::cout.imbue(std::locale("pt_BR.UTF8"));
-        std::cout.imbue(std::locale("C"));
-    }
-
-};
+using namespace std;
 
 int main(){
 
-    Produto meuObj;
-    meuObj.receberInfo();
-    meuObj.detalhes();
+    int x = 4;
+    
+    cout<<"\n";
+    cout<<(x>2 && x <10);
+    cout<<"\n";
+
 
     return 0;
 }
