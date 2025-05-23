3133196afc3d8bf4c7684eb77d72b45ea776c45c
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-23 20:56:57 -0300

    string

--- teste.cpp
+++ teste.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 int main(){
 
-    int x = 4;
-    
-    cout<<"\n";
-    cout<<(x>2 && x <10);
-    cout<<"\n";
+    string nome = "Computador";
+
+    cout<<"Eu gosto de " + nome;
 
 
     return 0;
