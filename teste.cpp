3ebe1d141a880d356868df54255ae4eb7b4b4e10
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-24 10:36:47 -0300

    if

--- teste.cpp
+++ teste.cpp
@@ -2,10 +2,15 @@
 
 int main(){
 
-    if(4 > 2){
-        std::cout<<"Sim"<<std::endl;
+    int idade;
+
+    std::cout<<"Digite sua idade: "<<std::endl;
+    std::cin>>idade;
+
+    if(idade >= 18){
+        std::cout<<"Você é maior de idade!"<<std::endl;
     }else{
-        std::cout<<"Não!"<<std::endl;
+        std::cout<<"Você é menor de idade!"<<std::endl;
     }
 
     return 0;
