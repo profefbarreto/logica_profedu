24710abb2ef91873bde23d34e46d47d5c829321b
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-24 11:38:55 -0300

    double e config %.2

--- teste.c
+++ teste.c
@@ -2,9 +2,13 @@
 
 int main(){
 
-    int x, y, z;
-    x = y = z = 50;
-    printf("%d", x + y + z); 
+    int num1 = 5;
+    float num2 = 9.59;
+    double num3 = 3.78;
+
+    printf("%d", num1);
+    printf("\n%.2f", num2);
+    printf("\n%.2lf", num3);
 
     return 0;
 }
