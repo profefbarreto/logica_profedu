0531a2f71bf1ee9248a857db522b752dfc2b641a
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-26 10:23:53 -0300

    switch

--- teste.c
+++ teste.c
@@ -2,16 +2,37 @@
 
 int main() {
     
-    char nome[50];
-    int idade;
+    int dia;
 
-    printf("Digite o nome: ");
-    scanf("%s", &nome);
+    printf("Qual dia da semana você quer: ");
+    scanf("%d", &dia);
 
-    printf("Digite a idade: ");
-    scanf("%d", &idade);
-
-    printf("O nome é %s e tem %d anos. ", nome, idade);
+    switch(dia){
+        case 1:
+            printf("Domingo!");
+        break;
+        case 2:
+            printf("Segunda-feira!");
+        break;
+        case 3:
+            printf("Terça-feira!");
+        break;
+        case 4:
+            printf("Quarta-feira!");
+        break;
+        case 5:
+            printf("Quinta-feira!");
+        break;
+        case 6:
+            printf("Sexta-feira!");
+        break;
+        case 7:
+            printf("Sábado!");
+        break;
+        default:
+            printf("Nenhuma opção válida!");
+        break;
+    }
 
     return 0;
 }
