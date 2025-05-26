806bbddefbe9c9840a9c4d23a66fb35d87e9b47f
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-26 10:44:01 -0300

    While

--- teste.c
+++ teste.c
@@ -2,31 +2,11 @@
 
 int main() {
     
-    char meses[4][10] = {"Janeiro", "Fevereiro", "Março", "Abril"};
-    int mes;
+    int i = 0;
 
-    printf("Digite um mês: ");
-    scanf("%d", &mes);
-
-    switch(mes){
-        case 1: 
-            printf("Você escolheu %s.", meses[mes-1]);
-        break;
-        case 2:
-            printf("Você escolheu %s.", meses[mes-1]);
-        break;
-        case 3:
-            printf("Você escolheu %s.", meses[mes-1]);
-        break;
-        case 4:
-            printf("Você escolheu %s.", meses[mes-1]);
-        break;
-
-        //Seguindo essa lógica, vamos completar o código??
-
-        default:
-            printf("Nenhuma opção válida!!");
-        break;
+    while(i < 5){
+        printf("\n%d", i);
+        i++;
     }
 
     return 0;
