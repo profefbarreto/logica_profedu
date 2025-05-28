c22168e8a97926614578c5b212b7768ace8f3f4e
Eduardofbarreto <fbarreto.eduardo>
2025-05-27 21:57:04 -0300

    lógica com Python

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-pasteis = ["Carne", "Frango", "Queijo", "Brócolis", "Chocolate"]
-precoPasteis = [9.99, 7.99, 10.90, 8.99, 12.99]
+a = 4
+b = int(input("Digite um valor para b: "))
 
-pizzas = ["Queijo", "Calabresa", "Frango", "Portuguesa", "Doce"]
-precoPizzas = [15.90, 16.90, 12.90, 14.90, 19.90]
+soma = a + b
 
-bebida = ["Cola-Cola", "Pepsi", "Guaraná", "Sprite", "Fanta"]
-precoBebidas = [9.99, 7.99, 6.99, 7.99, 6.99]
+nome = "Eduardo"
+sobrenome = str(input("Digite seu sobrenome: "))
 
-total_pedido = 0.0
-
-print("============================")
-print("Bem-vindo ao nosso cardápio!")
-print("============================")
-
-int(input("Escolha seu pastel: "))
-for i in range(5):
-    print(f"{i+1} - {pasteis[i]} (R$ {precoPasteis[i]:.2f})")
-escolha_pastel_str = input("Sua opção de pastel: ")    
+print(f"Seu nome é {nome} e seu sobrenome é {sobrenome}")
+print(f"A soma é de {soma}")
