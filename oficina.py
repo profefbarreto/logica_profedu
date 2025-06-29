635f20815bd53e5f4f89f7c590416c15d70fe8d7
profefbarreto <fbarreto.eduardo@gmail.com>
2025-06-29 15:02:47 -0300

    tabuada

--- oficina.py
+++ oficina.py
@@ -1,6 +1,7 @@
-
+tab = int(input("Digite um número para fazer a tabuada: "))
 
 tabuada = [1,2,3,4,5,6,7,8,9,10]
 
 for x in tabuada:
-    print(x)
+    resultado = tab * x
+    print(f"O resultado de {tab} x {x} = {resultado}.")
