37da45ec8a06aeb18c326d1cccb20974c9d7d9b2
profefbarreto <fbarreto.eduardo@gmail.com>
2025-06-29 14:24:37 -0300

    usando f-string e concatenando

--- oficina.py
+++ oficina.py
@@ -1,4 +1,5 @@
 nome = "Eduardo"
+mensagem1 = "Olá meu nome é"
+mensagem2 = "e estou muito feliz de estar participando desta oficina!"
 
-
-print(f"Olá, meu nome é {nome}!")
+print(f"{mensagem1} {nome} {mensagem2}")
