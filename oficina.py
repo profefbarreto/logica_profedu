abdf8fb49520838e7918969523d3ad3f3d9277b1
profefbarreto <fbarreto.eduardo@gmail.com>
2025-06-29 14:45:41 -0300

    entrada de dados com int

--- oficina.py
+++ oficina.py
@@ -1,6 +1,8 @@
-a = input("Digite um valor para a: ")
-b = input("Digite um valor para b: ")
+a = int(input("Digite um valor para a: "))
+b = int(input("Digite um valor para b: "))
 
-soma = a + b
+soma = (a + b)*2
+
+#cuidado com essa resposta
 
 print(f"O resultado é {soma}")
